09410c3c0191d92242c43af322b5e25a847669d3
lilou <lilou@coocaa.com>
2021-09-06 10:41:43 +0800

    update: publish_to_beta.sh

--- publish_to_beta.sh
+++ publish_to_beta.sh
@@ -1,11 +1,11 @@
-npm run build
+# npm run build
 rm -rf ballcat-ui.tar.gz
 tar cvf ballcat-ui.tar.gz dist
-scp ballcat-ui.tar.gz root@106.75.185.163:/root/w/compose-hub/web-hub/ballcat/ballcat-ui.tar.gz
-ssh root@106.75.185.163 "cd /root/w/compose-hub/web-hub/ballcat/; rm -rf dist"
+scp ballcat-ui.tar.gz root@ballcat.host:/root/w/compose-hub/web-hub/ballcat/ballcat-ui.tar.gz
+ssh root@ballcat.host "cd /root/w/compose-hub/web-hub/ballcat/; rm -rf dist"
 
-ssh root@106.75.185.163 "cd /root/w/compose-hub/web-hub/ballcat/; tar -xvf ballcat-ui.tar.gz"
-ssh root@106.75.185.163 "cd /root/w/compose-hub/;docker-compose restart common-nginx"
+ssh root@ballcat.host "cd /root/w/compose-hub/web-hub/ballcat/; tar -xvf ballcat-ui.tar.gz"
+ssh root@ballcat.host "cd /root/w/compose-hub/;docker-compose restart common-nginx"
 rm -rf ballcat-ui.tar.gz
 
 
